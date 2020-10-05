1866448e69bcfcea090536f00cd19c87efc06962
ibnvladimir <49831763+ibnvladimir@users.noreply.github.com>
2020-10-05 11:52:57 +0400

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(arr, config) {
+  let stack = [];
+	
+	for (let i = 0; i < arr.length; i++){
+		let stackLength = stack.length;
+		
+		for (let j = 0; j < config.length; j++){
+			if (arr[i] === config[j][0]){
+				stack.push(arr[i]);
+				break;
+			}
+			
+			if(arr[i] === config[j][1]){
+				let temp = stack.pop();
+				  if(temp !== config[j][0]){
+				  	  return false;
+				  }
+				  break;
+			}
+		}
+		
+		if (stackLength === stack.length){
+			return false;
+		}
+		
+		
+	}
+	
+	return true;
 }
